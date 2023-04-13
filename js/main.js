877c402497075534a14c02d1039daf4013df792d
tedmosbious <79196430+tedmosbious@users.noreply.github.com>
2023-04-13 13:17:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,13 +7,12 @@ tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
-
+tg.MainButton.setText( 'Подтвердить' );
 var ADMIN_ID = 5866779912;
 var TOKEN = "5968261554:AAEgMw9yRV6_yIni3qUQOYZo72Q75PkabhA"
 
 userData = document.querySelector("div.header-buttom p b");
-alert(tg);
-console.log(tg.initData);
+alert("1");
 
 //userData.innerText = tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name + " (" + tg.initDataUnsafe.user.id + ")";
 
